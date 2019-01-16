fdbfd679d2b40d4941af9fb4b9c61857f183b01f
Juancarlos Rojas <jrojasv@movidreams.com>
2019-01-16 00:32:09 -0300

    Configuracion de la BD

--- test_agence/settings.py
+++ test_agence/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'test_agence.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_agence',
+        'USER': 'test_agence',
+        'PASSWORD': '',
+        'HOST': '127.0.01',
+        'PORT': '3306',
+
     }
 }
 
@@ -103,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es-cl'
 
 TIME_ZONE = 'UTC'
 
