ad28b06a8fecb5e983b92b9ce7d041fed5ccf881
Juancarlos Rojas <jrojasv@movidreams.com>
2019-01-16 23:22:31 -0300

    ADD: Archivos UI

--- apps/consultor/models.py
+++ apps/consultor/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+"""
 
 class CaoAcompanhamentoSistema(models.Model):
     co_acompanhamento = models.AutoField(primary_key=True)
@@ -1148,49 +1149,6 @@ class CaoValorDescanso(models.Model):
         db_table = 'cao_valor_descanso'
 
 
-class DjangoAdminLog(models.Model):
-    action_time = models.DateTimeField()
-    object_id = models.TextField(blank=True, null=True)
-    object_repr = models.CharField(max_length=200)
-    action_flag = models.PositiveSmallIntegerField()
-    change_message = models.TextField()
-    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
-    user = models.ForeignKey('AuthUser', models.DO_NOTHING)
-
-    class Meta:
-        managed = False
-        db_table = 'django_admin_log'
-
-
-class DjangoContentType(models.Model):
-    app_label = models.CharField(max_length=100)
-    model = models.CharField(max_length=100)
-
-    class Meta:
-        managed = False
-        db_table = 'django_content_type'
-        unique_together = (('app_label', 'model'),)
-
-
-class DjangoMigrations(models.Model):
-    app = models.CharField(max_length=255)
-    name = models.CharField(max_length=255)
-    applied = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_migrations'
-
-
-class DjangoSession(models.Model):
-    session_key = models.CharField(primary_key=True, max_length=40)
-    session_data = models.TextField()
-    expire_date = models.DateTimeField()
-
-    class Meta:
-        managed = False
-        db_table = 'django_session'
-
 
 class PermissaoSistema(models.Model):
     co_usuario = models.CharField(primary_key=True, max_length=20)
@@ -1215,3 +1173,5 @@ class TipoUsuario(models.Model):
         managed = False
         db_table = 'tipo_usuario'
         unique_together = (('co_tipo_usuario', 'co_sistema'),)
+
+"""
